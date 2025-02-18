f9554bfb0b7dc1efd1bc27b57c9adff3311bca0f
aakansha <aakanshahujare@gmail.com>
2025-02-18 11:16:52 +0000

    add

--- basic.py
+++ basic.py
@@ -1,2 +1,6 @@
 name=input("enter your name:")
-print("hell0 ",name)
+num1=int(input("enter the 1st number:"))
+num2=int(input("enter the 2nd number"))
+print("hello ",name)
+sum=num1+num2
+print("the sum of both number is ",sum)
